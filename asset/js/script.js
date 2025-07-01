2b7736cb28afb5dc24314f7eb355bb3d317665de
Maedeh Sadat Khorasani <maedeh.skh80@gmail.com>
2025-07-01 19:29:35 +0330

    deleting cdns

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,7 +1,44 @@
-const swiper = new Swiper(".swiper", {
+const swiper = new Swiper(".threecards", {
   // Optional parameters
   direction: "horizontal",
   loop: true,
+  slidesPerView: 1,
+  spaceBetween: 70,
+
+  breakpoints: {
+    600: {
+      slidesPerView: 2,
+    },
+    868: {
+      slidesPerView: 3,
+    },
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const swiperfourcards = new Swiper(".fourcards", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 70,
+
+  breakpoints: {
+    600: {
+      slidesPerView: 2,
+    },
+    868: {
+      slidesPerView: 3,
+    },
+    950: {
+      slidesPerView: 4,
+    },
+  },
 
   // Navigation arrows
   navigation: {
